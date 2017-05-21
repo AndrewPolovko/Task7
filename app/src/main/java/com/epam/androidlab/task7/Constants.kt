6bfd1ba1bedcfe180d62b8b7a9591d1bafce9c1b
AndrewPolovko <lion569@mail.ru>
2017-05-21 16:35:08 +0300

    -

---
+++ app/src/main/java/com/epam/androidlab/task7/Constants.kt
@@ -0,0 +1,12 @@
+package com.epam.androidlab.task7
+
+class Constants {
+    companion object{
+        val KEY_FOR_FILE_URL = "${BuildConfig.APPLICATION_ID}_file_url"
+        val KEY_FOR_FILE_NAME = "${BuildConfig.APPLICATION_ID}_file_name"
+        val KEY_FOR_DOWNLOADED_FILE_URI = "${BuildConfig.APPLICATION_ID}_downloaded_file_uri"
+
+        val TEST_FILE_URL = "https://media.giphy.com/media/6rGtUzLFJQlyw/giphy.gif"
+        val TEST_FILE_NAME = "test.gif"
+    }
+}
